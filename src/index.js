31edf40c87410aee897d8a5d4f490eebaf87507c
Alex <alex.stormwood@bigfootds.com>
2026-04-14 13:50:43 +1000

    feat: contact form toggle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("Local JS is running!");
+
+// Only run the jQuery when the document is fully loaded
+$(document).ready(function () {
+
+	// Find the "contact us!" button
+	$(".toggleButton").click(function () {
+		// Find the contact form and toggle it as well
+		$("form").toggle();
+	});
+});
